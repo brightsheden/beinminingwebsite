2770e59122bacb295c18d8a06e154a3371cc820b
shedenbright <shedenbright@gmail.com>
2023-12-22 18:02:07 -1200

    adding the build folder

--- backend/settings.py
+++ backend/settings.py
@@ -123,16 +123,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 """
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'mysql.connector.django',
@@ -147,7 +146,7 @@ DATABASES = {
     }
 }
 
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
